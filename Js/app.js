795632e72d1f54c52828b79406383d2983123531
Daniel Portillo <79211913+dportillo23@users.noreply.github.com>
2022-08-14 22:31:46 +0200

    Update images in app.js

--- Js/app.js
+++ Js/app.js
@@ -7,7 +7,7 @@ const wrapper = $('.nav--wrapper');
 hamburger.addEventListener('click', (e) => {
     const wrapperClasses = wrapper.classList
     wrapperClasses.toggle('active', !wrapperClasses.contains('active'));
-    hamburger.src = !wrapperClasses.contains('active') ? '../images/icon-menu.svg' : '../images/icon-close-menu.svg'  
+    hamburger.src = !wrapperClasses.contains('active') ? 'images/icon-menu.svg' : 'images/icon-close-menu.svg'  
 })
 
 const arrows = $$('.arrow');
@@ -16,7 +16,7 @@ arrows.forEach(arrow => {
     arrow.addEventListener('click', e => {
         const subSectionClasses = e.target.parentElement.lastElementChild.classList
         subSectionClasses.toggle('expanded', !subSectionClasses.contains('expanded'));
-        arrow.src = !subSectionClasses.contains('expanded') ? './images/icon-arrow-down.svg' : './images/icon-arrow-up.svg'  
+        arrow.src = !subSectionClasses.contains('expanded') ? 'images/icon-arrow-down.svg' : 'images/icon-arrow-up.svg'  
 
     })
 })
